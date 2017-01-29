3b851fbb742392f8b55e9511f62b0a5c5803b089
Lucho <lucho.farfan9@gmail.com>
2017-01-28 22:47:53 -0500

    added babel dir compiler

---
+++ static/src/usuarios/usuarios.roles.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var UsuariosRoles = function UsuariosRoles() {
+    _classCallCheck(this, UsuariosRoles);
+};
+
+console.log('Compilo!!!');
+console.log('hola');
